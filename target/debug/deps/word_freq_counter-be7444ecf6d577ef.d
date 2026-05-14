f0cfa3756734385eb175edf57527873c350807f4
Souradip Pal <souradip1000@gmail.com>
2026-05-15 00:09:57 +0530

    added readme

---
+++ target/debug/deps/word_freq_counter-be7444ecf6d577ef.d
@@ -0,0 +1,8 @@
+/home/souradip/Code/word-freq-counting-rust/word-freq-counter/target/debug/deps/word_freq_counter-be7444ecf6d577ef.d: src/main.rs src/reader.rs src/counter.rs src/display.rs
+
+/home/souradip/Code/word-freq-counting-rust/word-freq-counter/target/debug/deps/word_freq_counter-be7444ecf6d577ef: src/main.rs src/reader.rs src/counter.rs src/display.rs
+
+src/main.rs:
+src/reader.rs:
+src/counter.rs:
+src/display.rs:
